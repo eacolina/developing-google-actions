2afbd30770f56171706d45ccfe211e518ee3e159
Edu Colina <educolina@Edus-MacBook-Air.local>
2017-02-16 11:15:18 -0500

    Fixed action Map

--- server/express_webhook.js
+++ server/express_webhook.js
@@ -19,7 +19,6 @@
     function runMethod(assistant){
       var id = assistant.getArgument('method_ID');
       Meteor.call('random.insert', id);
-// <<<<<<< HEAD
       assistant.tell("Ok, method", method_ID, "is now running");
     }
 
@@ -27,32 +26,11 @@
       var view = assistant.getArgument("view");
       NavData.insert({ name: view, createdAt: new Date() });
       assistant.tell("You can now see ", view, " in your device." )
-// =======
-//       assistant.tell("Ok method ", id, "is now running!");
-// >>>>>>> master
     }
 
-    //
-    // function goHome(assistant){
-    //   NavData.insert({ name: 'Home', createdAt: new Date() } );
-    //   assistant.tell("Ok, you should now be able to see the home page on your device");
-    // }
-    //
-    //
-    // function goDetail1(assistant){
-    //   NavData.insert({ name: 'Detail 1', createdAt: new Date() } );
-    //   assistant.tell("Ok, you should now be able to see the Detail 1 page on your device");
-    // }
-    //
-    // function goDetail2(assistant){
-    //   NavData.insert({ name: 'Detail 2', createdAt: new Date() } );
-    //   assistant.tell("Ok, you should now be able to see the Detail 2 page on your device");
-    // }
-
     actionMap.set('input.welcome',welcomeIntent);
+    actionMap.set('goToView_intent', changeView)
     actionMap.set('runMethod_intent', runMethod);
-    actionMap.set('goHome_intent', goHome);
-    actionMap.set('goDetail1_intent', goDetail1);
-    actionMap.set('gDetail2_intent', goDetail2);
+
     assistant.handleRequest(actionMap);
   })
