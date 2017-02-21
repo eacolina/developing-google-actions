10e01d39f05bfd92c0372e0f8d6e28c5c1a1a5e1
Michael Yaworski <mikeyaworski@hotmail.com>
2017-02-21 14:00:22 -0500

    Formatting

--- app/server/express_webhook.js
+++ app/server/express_webhook.js
@@ -8,24 +8,27 @@
   });
 
   app.post('/ga-webhook', function(req, res){
-    const assistant = new ApiAiAssistant({request:req, response:res}); // create new instance of API assistan with the reponse and request of this particular endpoint
+    const assistant = new ApiAiAssistant({request:req, response:res}); // create new instance of ApiAiAssistant according to the request given
 
-    function welcomeIntent(assistant){ // create a function for the welcomeIntent
+    // function for the welcomeIntent
+    function welcomeIntent(assistant) {
       assistant.tell("Welcome to the Sample Action that controls a Meteor App");
     }
 
-    function runMethod(assistant){ // function to run a method when the 'runMethod_intent is called'
-      var id = assistant.getArgument('method_ID'); // use .getArgument('parameterName') to get the value of a paramter
+    // function to run a method when the 'runMethod_intent' is called
+    function runMethod(assistant) {
+      var id = assistant.getArgument('method_ID'); // use .getArgument(parameterName) to get the value of a paramter (parameterNames defined in API.ai) 
       if (id) {
         Meteor.call('random.insert', id); // insert a method into the database
-        assistant.tell("Ok method " + id + " is now running!"); //  using .tell to talk to the user and finish this conversation
+        assistant.tell("Ok method " + id + " is now running!"); // using .tell to talk to the user and finish this conversation
       } else {
         assistant.tell("There was an error with your request.");
       }
     }
 
-    function Navigate(assistant){ // function to navigate to a certain view when th 'goToView_intent' is called
-      var view = assistant.getArgument('view'); // get the value of 'view'
+    // function to navigate to a certain view when the 'goToView_intent' is called
+    function Navigate(assistant) {
+      var view = assistant.getArgument('view'); // get the value of the 'view' parameter
       Meteor.call('insertView', view); // insert the new view in NavData
       assistant.tell("Ok you can now see " + view + " on your device"); // inform the user that process is done
     }
